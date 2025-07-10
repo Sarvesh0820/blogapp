46415b3fa2cbd2df1362b621c0b449251010561c
sarvesh0820 <sarveshhadgaonkar01@gmail.com>
2025-07-11 03:00:31 +0530

    a

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import {Logo} from "../../components/index"
 
 const Footer = () => {
   return (
-     <section className="relative overflow-hidden py-10 bg-black  border border-t-2 border-t-gray-200">
+     <section className="relative overflow-hidden py-10 bg-black ">
             <div className="relative z-10 mx-auto max-w-7xl px-4">
                 <div className="-m-6 flex flex-wrap">
                     <div className="w-full p-6 md:w-1/2 lg:w-5/12">
