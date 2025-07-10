27789506a0768c01514ef8d6de3254d4547060f4
sarvesh0820 <sarveshhadgaonkar01@gmail.com>
2025-07-10 21:11:11 +0530

    l

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import logo from "../assets/logo.png"
 
-const Logo = (width = "100px") => {
+const Logo = (width = "10px") => {
   return (
     <img src={logo} alt="Logo" />
   )
